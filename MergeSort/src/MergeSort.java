6fe2fcd3f677ec71974bab50f53a2c767d428998
Iván Pérez Huete <ivan120695@gmail.com>
2018-09-30 10:17:56 +0200

    Done

--- MergeSort/src/MergeSort.java
+++ MergeSort/src/MergeSort.java
@@ -11,7 +11,7 @@ public class MergeSort {
 		
 		int elem = consola.nextInt();
 		int [] array = new int [elem];
-		int last = array.length;
+		int length = array.length;
 		
 		for (int x=0; x<elem; x++) {
 		    System.out.println("Introduzca el elemento [" + x + "]");
@@ -21,7 +21,7 @@ public class MergeSort {
 		System.out.println("Continuamos con el algoritmo. Su array inicial es: " + Arrays.toString(array) + " Lo vamos a ordenar. " );
 		
 		MergeSort ejercicio = new MergeSort();
-		ejercicio.sort(array, 0, last);
+		ejercicio.sort(array, 0, length-1);
 		
 		System.out.println("Su array ordenado es: ");
 		ejercicio.mostrar(array);
@@ -29,7 +29,6 @@ public class MergeSort {
 	}
 	
 	public void merge (int [] array, int ini, int mitad , int last) { // Ordena las dos mitades 
-		
 		
 		int [] arrayAux = new int [array.length]; // Array auxiliar que va almacenando
 		for (int i = ini; i <= last; i++) {
@@ -71,8 +70,11 @@ public class MergeSort {
 	}
 	
 	public void mostrar (int [] arrayMuestra) {
+		
 		int n = arrayMuestra.length;
-		for (int i=0; i<= n; i++) {
+		
+		//Recorrer hasta menos 1 que sino se va de length
+		for (int i=0; i<= n-1; i++) {
 			System.out.println(arrayMuestra[i]);
 		}
 	}
